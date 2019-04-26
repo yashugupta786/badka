ff19af73216705dce365e2262a4f262d51206e72
Yashu Gupta <yashugupta.gupta11@gmail.com>
2019-04-26 17:06:30 +0530

    Create DocSim.py

---
+++ DocSim.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class DocSim(object):
+    def __init__(self, w2v_model , stopwords=[]):
+        self.w2v_model = w2v_model
+        self.stopwords = stopwords
+
+    def vectorize(self, doc):
+       
+        doc = doc.lower()
+        words = [w for w in doc.split(" ") if w not in self.stopwords]
+        word_vecs = []
+        for word in words:
+            try:
+                vec = self.w2v_model[word]
+                word_vecs.append(vec)
+            except KeyError:
+           
+                pass
+
+        
+        vector = np.mean(word_vecs, axis=0)
+        return vector
+
+
+    def _cosine_sim(self, vecA, vecB):
+        """Find the cosine similarity distance between two vectors."""
+        csim = np.dot(vecA, vecB) / (np.linalg.norm(vecA) * np.linalg.norm(vecB))
+        if np.isnan(np.sum(csim)):
+            return 0
+        return csim
+
+    def calculate_similarity(self, source_doc, target_docs=[], threshold=0.7):
+        
+        if isinstance(target_docs, str):
+            target_docs = [target_docs]
+
+        source_vec = self.vectorize(source_doc)
+        results = []
+        for doc in target_docs:
+            target_vec = self.vectorize(doc)
+            sim_score = self._cosine_sim(source_vec, target_vec)
+            if sim_score > threshold:
+                results.append({
+                    'score' : sim_score,
+                    'doc' : doc
+                })
+            results.sort(key=lambda k : k['score'] , reverse=True)
+
+        return results
+
+        
